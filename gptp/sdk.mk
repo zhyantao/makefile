73625057c69e19f54bfb80f5b5a649fa2ae50c19
Yantao Zhang <yantao.z@outlook.com>
2024-12-05 11:00:54 +0800

    Delete `export` cause we don not need it

--- gptp/sdk.mk
+++ gptp/sdk.mk
@@ -3,29 +3,29 @@ CURR_DIR := $(shell pwd)
 BUILDROOT_DIR := $(HOME)/toolchain/buildroot/arm-buildroot-linux-gnueabihf_sdk-buildroot
 SYSROOT_DIR := $(BUILDROOT_DIR)/arm-buildroot-linux-gnueabihf/sysroot
 TOOLCHAIN_DIR := $(BUILDROOT_DIR)/bin
-export PATH := $(TOOLCHAIN_DIR):$(PATH)
+PATH := $(TOOLCHAIN_DIR):$(PATH)
 
 # cross compile options
-export ARCH := arm
-export CROSS_COMPILE := arm-buildroot-linux-gnueabihf-
+ARCH := arm
+CROSS_COMPILE := arm-buildroot-linux-gnueabihf-
 
-export CC := $(CROSS_COMPILE)gcc
-# export CXX := $(CROSS_COMPILE)g++
-export AS := $(CROSS_COMPILE)as
-export LD := $(CROSS_COMPILE)ld
-export STRIP := $(CROSS_COMPILE)strip
-export RANLIB := $(CROSS_COMPILE)ranlib
-export OBJCOPY := $(CROSS_COMPILE)objcopy
-export OBJDUMP := $(CROSS_COMPILE)objdump
-export AR := $(CROSS_COMPILE)ar
-export NM := $(CROSS_COMPILE)nm
+CC := $(CROSS_COMPILE)gcc
+# CXX := $(CROSS_COMPILE)g++
+AS := $(CROSS_COMPILE)as
+LD := $(CROSS_COMPILE)ld
+STRIP := $(CROSS_COMPILE)strip
+RANLIB := $(CROSS_COMPILE)ranlib
+OBJCOPY := $(CROSS_COMPILE)objcopy
+OBJDUMP := $(CROSS_COMPILE)objdump
+AR := $(CROSS_COMPILE)ar
+NM := $(CROSS_COMPILE)nm
 
-export CFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
-# export CXXFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
-export LDFLAGS := -L$(SYSROOT_DIR)/lib -L$(SYSROOT_DIR)/usr/lib
+CFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
+# CXXFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
+LDFLAGS := -L$(SYSROOT_DIR)/lib -L$(SYSROOT_DIR)/usr/lib
 
-export PKG_CONFIG_DIR := "$(SYSROOT_DIR)/usr/lib/pkgconfig"
-export PKG_CONFIG_PATH := "$(PKG_CONFIG_DIR):$(SYSROOT_DIR)/usr/share/pkgconfig"
-export PKG_CONFIG_LIBDIR := "$(PKG_CONFIG_DIR)"
-export PKG_CONFIG_SYSROOT_DIR := "$(SYSROOT_DIR)"
-export PKG_CONFIG_DISABLE_UNINSTALLED := "yes"
+PKG_CONFIG_DIR := "$(SYSROOT_DIR)/usr/lib/pkgconfig"
+PKG_CONFIG_PATH := "$(PKG_CONFIG_DIR):$(SYSROOT_DIR)/usr/share/pkgconfig"
+PKG_CONFIG_LIBDIR := "$(PKG_CONFIG_DIR)"
+PKG_CONFIG_SYSROOT_DIR := "$(SYSROOT_DIR)"
+PKG_CONFIG_DISABLE_UNINSTALLED := "yes"
