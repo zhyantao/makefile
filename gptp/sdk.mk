533174ee6b339715dcab6ac9961a3917a95b2ad0
Yantao Zhang <yantao.z@outlook.com>
2024-12-05 15:35:16 +0800

    Change the `SYSROOT_DIR` to the `SYSROOT`

--- gptp/sdk.mk
+++ gptp/sdk.mk
@@ -1,7 +1,7 @@
 CURR_DIR := $(shell pwd)
 
 BUILDROOT_DIR := $(HOME)/toolchain/buildroot/arm-buildroot-linux-gnueabihf_sdk-buildroot
-SYSROOT_DIR := $(BUILDROOT_DIR)/arm-buildroot-linux-gnueabihf/sysroot
+SYSROOT := $(BUILDROOT_DIR)/arm-buildroot-linux-gnueabihf/sysroot
 TOOLCHAIN_DIR := $(BUILDROOT_DIR)/bin
 PATH := $(TOOLCHAIN_DIR):$(PATH)
 
@@ -20,12 +20,12 @@ OBJDUMP := $(CROSS_COMPILE)objdump
 AR := $(CROSS_COMPILE)ar
 NM := $(CROSS_COMPILE)nm
 
-CFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
-# CXXFLAGS := --sysroot=$(SYSROOT_DIR) -I$(SYSROOT_DIR)/usr/include -g -Wall
-LDFLAGS := -L$(SYSROOT_DIR)/lib -L$(SYSROOT_DIR)/usr/lib
+CFLAGS := --sysroot=$(SYSROOT) -I$(SYSROOT)/usr/include -g -Wall
+# CXXFLAGS := --sysroot=$(SYSROOT) -I$(SYSROOT)/usr/include -g -Wall
+LDFLAGS := -L$(SYSROOT)/lib -L$(SYSROOT)/usr/lib
 
-PKG_CONFIG_DIR := "$(SYSROOT_DIR)/usr/lib/pkgconfig"
-PKG_CONFIG_PATH := "$(PKG_CONFIG_DIR):$(SYSROOT_DIR)/usr/share/pkgconfig"
+PKG_CONFIG_DIR := "$(SYSROOT)/usr/lib/pkgconfig"
+PKG_CONFIG_PATH := "$(PKG_CONFIG_DIR):$(SYSROOT)/usr/share/pkgconfig"
 PKG_CONFIG_LIBDIR := "$(PKG_CONFIG_DIR)"
-PKG_CONFIG_SYSROOT_DIR := "$(SYSROOT_DIR)"
+PKG_CONFIG_SYSROOT := "$(SYSROOT)"
 PKG_CONFIG_DISABLE_UNINSTALLED := "yes"
